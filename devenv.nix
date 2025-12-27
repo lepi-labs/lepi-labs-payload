65b4528957bf8d12556f8d642b779e01f8d1475a
prophetofxenu <xenu@lepi-labs.com>
2025-12-27 13:14:39 -0500

    initial commit

---
+++ devenv.nix
@@ -0,0 +1,22 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  dotenv.enable = true;
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    nodejs
+    pnpm
+  ];
+
+  # https://devenv.sh/services/
+  services.mongodb.enable = true;
+
+  # https://devenv.sh/tests/
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
