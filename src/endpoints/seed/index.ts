ebab2335dfc64f793becc02fb4d4ac7e3ddd0616
prophetofxenu <xenu@lepi-labs.com>
2026-03-26 21:09:55 -0400

    Dockerfile and build

--- src/endpoints/seed/index.ts
+++ src/endpoints/seed/index.ts
@@ -93,7 +93,7 @@ export const seed = async ({
       payload.updateGlobal({
         slug: global,
         data: {
-          navItems: [],
+          //navItems: [],
         },
         depth: 0,
         context: {
--- src/payload-types.ts
+++ src/payload-types.ts
@@ -139,9 +139,10 @@ export interface Config {
     socialmedialink: SocialmedialinkSelect<false> | SocialmedialinkSelect<true>;
   };
   locale: null;
-  user: User & {
-    collection: 'users';
+  widgets: {
+    collections: CollectionsWidget;
   };
+  user: User;
   jobs: {
     tasks: unknown;
     workflows: unknown;
@@ -221,6 +222,7 @@ export interface User {
       }[]
     | null;
   password?: string | null;
+  collection: 'users';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
@@ -1964,6 +1966,16 @@ export interface SocialmedialinkSelect<T extends boolean = true> {
   updatedAt?: T;
   createdAt?: T;
   globalType?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "collections_widget".
+ */
+export interface CollectionsWidget {
+  data?: {
+    [k: string]: unknown;
+  };
+  width: 'full';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
