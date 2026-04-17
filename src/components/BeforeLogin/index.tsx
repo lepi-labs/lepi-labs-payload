83464170bc4c71802b80edd9b978d54fb595f6d9
prophetofxenu <xenu@lepi-labs.com>
2026-04-17 19:54:25 -0400

    remove Payload notes

--- src/components/BeforeLogin/index.tsx
+++ src/components/BeforeLogin/index.tsx
@@ -4,10 +4,7 @@ export const BeforeLogin: React.FC = () => {
   return (
     <div>
       <p>
-        <b>Welcome to your dashboard!</b>
-        {' This is where site admins will log in to manage your store. Customers will need to '}
-        <a href={`${process.env.PAYLOAD_PUBLIC_SERVER_URL}/login`}>log in to the site instead</a>
-        {' to access their user account, order history, and more.'}
+        <b>authorized users only!</b>
       </p>
     </div>
   )
--- src/components/forms/ForgotPasswordForm/index.tsx
+++ src/components/forms/ForgotPasswordForm/index.tsx
@@ -6,7 +6,6 @@ import { Message } from '@/components/Message'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import Link from 'next/link'
 import React, { Fragment, useCallback, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
@@ -54,8 +53,7 @@ export const ForgotPasswordForm: React.FC = () => {
           <div className="prose dark:prose-invert mb-8">
             <p>
               {`Please enter your email below. You will receive an email message with instructions on
-              how to reset your password. To manage your all users, `}
-              <Link href="/admin/collections/users">login to the admin dashboard</Link>.
+              how to reset your password.`}
             </p>
           </div>
           <form className="max-w-lg" onSubmit={handleSubmit(onSubmit)}>
